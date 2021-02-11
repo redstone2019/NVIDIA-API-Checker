cba922f88cf744c6a002e2ad029815a5a1861dfd
redstone2019 <50508889+redstone2019@users.noreply.github.com>
2021-02-11 10:03:52 -0800

    Update WSC.go
    version 0.5

--- WSC.go
+++ WSC.go
@@ -11,17 +11,20 @@ func main() {
 	var svar string
 	flag.StringVar(&svar, "url", "https://google.com", "a url to check needs to be inputed here")
 	flag.Parse()
-	fmt.Println("Welcome to the NVIDIA API Checker v0.4 by redstone2019.")
+	fmt.Println("Welcome to the NVIDIA API Checker v0.5 by redstone2019.")
 	fmt.Println("This will loop forever giving HTTP Status Codes.")
-	fmt.Println("Please wait. (Usually takes aroung 10-30 seconds per loop)")
+	fmt.Println("Please wait. (Usually takes around 1 seconds per loop for fast websites, slow websites may take much longer)")
 	fmt.Println("URL:", svar)
+	time.Sleep(5 * time.Second)
 	for {
+		fmt.Println("Pinging...")
 		resp, err := http.Get(svar)
 		now := time.Now()
 		// Print the HTTP Status Code and Status Name
-		fmt.Println(err)
 		fmt.Println(now)
 		fmt.Println("HTTP Response Status:", resp.StatusCode, http.StatusText(resp.StatusCode))
-		time.Sleep(10 * time.Second)
+		fmt.Println("Error?", err)
+		fmt.Println(" ")
+		time.Sleep(1 * time.Second)
 	}
 }
