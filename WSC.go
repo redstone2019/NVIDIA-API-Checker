f4bcb7191c9c08fa558c3fd1ffdb4b525c3214e0
redstone2019 <50508889+redstone2019@users.noreply.github.com>
2021-03-08 14:32:51 -0800

    Update WSC.go
    more patching and clarification

--- WSC.go
+++ WSC.go
@@ -14,9 +14,10 @@ func main() {
 	fmt.Println("Welcome to the NVIDIA API Checker v0.51 by redstone2019.")
 	fmt.Println("This will loop forever giving HTTP Status Codes.")
 	fmt.Println("Please wait. (Usually takes around 1 second per loop for fast websites, slow websites may take much longer)")
-	fmt.Println("Warning you must include either https:// or http:// before your domain or you will encounter a panic runtime error")
+	fmt.Println("Warning you must include either https:// or http:// before your domain when using the URL variable/flag or you will encounter a panic runtime error")
 	fmt.Println("URL:", svar)
 	time.Sleep(5 * time.Second)
+	fmt.Println("")
 	for {
 		fmt.Println("Pinging...")
 		resp, err := http.Get(svar)
