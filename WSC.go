952c78e65bdae5cf95013ab57e2e8e7eabb5401f
redstone2019 <50508889+redstone2019@users.noreply.github.com>
2021-03-08 14:13:14 -0800

    Update WSC.go
    patching

--- WSC.go
+++ WSC.go
@@ -11,9 +11,10 @@ func main() {
 	var svar string
 	flag.StringVar(&svar, "url", "https://google.com", "a url to check needs to be inputed here")
 	flag.Parse()
-	fmt.Println("Welcome to the NVIDIA API Checker v0.5 by redstone2019.")
+	fmt.Println("Welcome to the NVIDIA API Checker v0.51 by redstone2019.")
 	fmt.Println("This will loop forever giving HTTP Status Codes.")
-	fmt.Println("Please wait. (Usually takes around 1 seconds per loop for fast websites, slow websites may take much longer)")
+	fmt.Println("Please wait. (Usually takes around 1 second per loop for fast websites, slow websites may take much longer)")
+	fmt.Println("Warning you must include either https:// or http:// before your domain or you will encounter a panic runtime error")
 	fmt.Println("URL:", svar)
 	time.Sleep(5 * time.Second)
 	for {
